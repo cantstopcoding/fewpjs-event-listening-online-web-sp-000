06ad3c1e6d63995fe95aa2b398796849ea1f22d6
Jonathan Koss <jonathan_koss@yahoo.com>
2020-10-01 17:11:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-function addingEventListener() {
-  function addingEventListener() {
-    const input = document.getElementById('input');
-    input.addEventListener('click', function(event) {
-        alert('I was clicked!');
-    });
-}
+const input = document.querySelector('input');
 
+function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
 }
